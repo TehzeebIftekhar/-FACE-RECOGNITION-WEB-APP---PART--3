dc0f989f4f8016532e8aac179bda7605dc4649d5
TehzeebIftekhar <98819400+TehzeebIftekhar@users.noreply.github.com>
2022-09-19 19:07:16 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/NBQ5jR5tX/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model is Loaded');
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_family_member_name").innerHTML = results[0].label;
+        document.getElementById("result_family_member_acuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
